19fe68ed7ca0771384bd11b5127d5d1cf9621403
Emerson Vergara <ee-web@hotmail.com>
2022-03-15 21:45:21 -0500

    Configuramos el uso de la base de datos instalamos la dependencia  npm install --save @nestjs/typeorm typeorm mysql2 y configuramos app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesController } from './messages/messages.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'nest',
+      database: 'sendmeapp_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, MessagesController],
   providers: [AppService],
 })
